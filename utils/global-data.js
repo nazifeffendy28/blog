8343f7c5d3ca4706eccc6a4c748c4b386ffa2f56
nazifeffendy28 <nazifeffendy28@gmail.com>
2024-10-16 10:10:15 +0700

    Add files via upload

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Nazif Hamza Effendy';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Tren Teknologi Informasi';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Nazif Hamza Effendy Rights.';
 
   return {
     name,
